ea30fa576dca4b380d2b534ab8ad715b30080ea1
tnhn60clk <130980811+tnhn60clk@users.noreply.github.com>
2024-05-04 23:44:08 +0300

    Update TunaSploit.py

--- TunaSploit.py
+++ TunaSploit.py
@@ -9,8 +9,14 @@ def signal_handler(sig, frame):
     print('Ctrl-C basıldı, program kapatılıyor...')
     sys.exit(0)
 
-# SIGINT sinyalini bu handler'a yönlendirin
+# SIGTSTP (ctrl-t) sinyalini yakalamak için bir handler fonksiyonu tanımlayın
+def sigtstp_handler(sig, frame):
+    print('Ctrl-T basıldı, TunaSploit shell\'ine geri dönülüyor...')
+    tunasploit_shell()
+
+# SIGINT ve SIGTSTP sinyallerini bu handler'lara yönlendirin
 signal.signal(signal.SIGINT, signal_handler)
+signal.signal(signal.SIGTSTP, sigtstp_handler)
 
 # Ekranı temizleme fonksiyonu
 def clear_screen():
@@ -26,50 +32,31 @@ def banner_yukle():
     secilen_banner = random.choice(banners)
     print(secilen_banner)
 
-# TunaSploit shell'i başlat
-def tunasploit_shell():
-    while True:
-        komut = input("TunaSploit> ")
-        if komut == 'exit':
-            break
-        elif komut == 'clear':
-            clear_screen()
-        elif komut == 'banner':
-            banner_yukle()
-        elif komut == 'opsiyon':
-            print("1: arp-scan\n2: nmap taraması\n3: dirb\n4: metasploit taraması")
-            secim = input("Seçiminizi girin: ")
-            if secim == '1':
-                arp_scan_tara_ve_yazdir()
-            elif secim == '2':
-                hedef_ip = input("Lütfen nmap taraması yapılacak hedef IP adresini girin: ")
-                nmap_parametreleri = input("Eğer ekstra nmap taraması parametreleri kullanmak isterseniz girin (örn: -A -T4), yoksa boş bırakın: ")
-                nmap_tarama(hedef_ip, nmap_parametreleri)
-            elif secim == '3':
-                hedef_ip = input("Lütfen dirb taraması yapılacak hedef IP adresini girin: ")
-                dirb_parametreleri = input("dirb için ekstra parametreler girin (örn: -w -l), yoksa boş bırakın: ")
-                dirb_calistir(hedef_ip, dirb_parametreleri)
-            elif secim == '4':
-                metasploit_arama()
-        else:
-            print(f"'{komut}' komutu tanınmadı.")
+# arp-scan ile ağdaki cihazları tara ve ekrana yazdır
+def arp_scan_tara_ve_yazdir():
+    try:
+        print("arp-scan komutu çalıştırılıyor...")
+        sonuc = subprocess.check_output(['arp-scan', '-l'], text=True)
+        print(sonuc)
+    except subprocess.CalledProcessError as e:
+        print(f"Hata: {e}")
 
 # nmap ile tarama yap ve sonuçları ekrana yazdır
 def nmap_tarama(ip, parametreler=None):
     try:
         komut = ['nmap', ip]
         if parametreler:
-            komut = ['nmap'] + parametreleri.split() + [ip]
+            komut = ['nmap'] + parametreler.split() + [ip]
         print(f"{' '.join(komut)} komutu çalıştırılıyor...")
-        sonuc = subprocess.run(komut, text=True, capture_output=True)
-        print(sonuc.stdout)
-        if "80/tcp open" in sonuc.stdout:
+        sonuc = subprocess.check_output(komut, text=True)
+        print(sonuc)
+        if "80/tcp open" in sonuc:
             dirb_calistir = input("80 portu açık algılandı. dirb aracını çalıştırmak ister misiniz? (E/H): ")
             if dirb_calistir.lower() == 'e':
                 dirb_parametreleri = input("dirb için ekstra parametreler girin (örn: -w -l), yoksa boş bırakın: ")
                 dirb_calistir(ip, dirb_parametreleri)
     except subprocess.CalledProcessError as e:
-        print(f"Hata: {e.output}")
+        print(f"Hata: {e}")
 
 # dirb ile dizin taraması yap ve arka planda çalıştır
 def dirb_calistir(ip, parametreleri=None):
@@ -90,10 +77,40 @@ def metasploit_arama():
     arama_sorgusu = input("Lütfen Metasploit'te aramak istediğiniz versiyonu girin: ")
     try:
         print(f"msfconsole -x 'search name:{arama_sorgusu}; exit' komutu çalıştırılıyor...")
-        sonuc = subprocess.run(['msfconsole', '-q','-x', f"search name:{arama_sorgusu}; exit"], text=True, capture_output=True)
-        print(sonuc.stdout)
+        sonuc = subprocess.check_output(['msfconsole', '-q','-x', f"search name:{arama_sorgusu}; exit"], text=True)
+        print(sonuc)
     except subprocess.CalledProcessError as e:
-        print(f"Hata: {e.output}")
+        print(f"Hata: {e}")
+
+# TunaSploit shell'i başlat
+def tunasploit_shell():
+    while True:
+        komut = input("TunaSploit> ")
+        if komut == 'exit':
+            break
+        elif komut == 'clear':
+            clear_screen()
+        elif komut == 'banner':
+            banner_yukle()
+        elif komut in ['1', '2', '3', '4']:
+            islem_sec(komut)
+        else:
+            print(f"'{komut}' komutu tanınmadı.")
+
+# İşlem seçme fonksiyonu
+def islem_sec(secim):
+    if secim == '1':
+        arp_scan_tara_ve_yazdir()
+    elif secim == '2':
+        hedef_ip = input("Lütfen nmap taraması yapılacak hedef IP adresini girin: ")
+        nmap_parametreleri = input("Eğer ekstra nmap taraması parametreleri kullanmak isterseniz girin (örn: -A -T4), yoksa boş bırakın: ")
+        nmap_tarama(hedef_ip, nmap_parametreleri)
+    elif secim == '3':
+        hedef_ip = input("Lütfen dirb taraması yapılacak hedef IP adresini girin: ")
+        dirb_parametreleri = input("dirb için ekstra parametreler girin (örn: -w -l), yoksa boş bırakın: ")
+        dirb_calistir(hedef_ip, dirb_parametreleri)
+    elif secim == '4':
+        metasploit_arama()
 
 # Ana fonksiyon
 if __name__ == "__main__":
